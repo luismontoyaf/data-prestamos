efccd57892501e334a330437f65315655b327c74
luismontoyaf <luisc1999lm@gmail.com>
2022-07-30 12:03:30 -0500

    Actualizacion logo y detalles visuales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/components/login/login.component';
 import { ResetpasswordComponent } from './pages/auth/components/resetpassword/resetpassword.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: "login", component: LoginComponent},
-  {path: "resetpassword", component: ResetpasswordComponent}
+  {path: "resetpassword", component: ResetpasswordComponent},
+  {path: 'home', component: HomeComponent}
 
 ];
 
