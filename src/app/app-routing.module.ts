77187fa7cab3686b2a6105d902a2a34b857882b5
luismontoyaf <luisc1999lm@gmail.com>
2022-08-21 22:09:29 -0500

    correcion rutas:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/auth/components/login/login.component';
-import { ResetpasswordComponent } from './pages/auth/components/resetpassword/resetpassword.component';
-import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from '../app/modules/auth/pages/login/login.component';
+import { ResetpasswordComponent } from '../app/modules/auth/pages/resetpassword/resetpassword.component';
+import { HomeComponent } from '../app/modules/home/pages/home-page/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
